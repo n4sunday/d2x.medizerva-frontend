733075a6183e9d9ef5582d459a335ec84e04fb4a
n4s <s5935512004@phuket.psu.ac.th>
2019-06-16 22:42:52 +0700

    Help layout

--- pages/help.js
+++ pages/help.js
@@ -4,8 +4,14 @@ const Help = () => {
     return (
         <div>
             <Layout t="barWhite" c="barWhite">
-                <div className="help pt-">
-
+                <div className="Help pt-50">
+                    <div className="container-fluid">
+                        <div className="row">
+                            <div className="history pa-0 col-xs-12 col-sm-12 col-md-6 col-lg-4">History</div>
+                            <div className="help pa-0 col-xs-12 col-sm-12 col-md-6 col-lg-4">Help</div>
+                            <div className="hospital-pill pa-0 col-xs-12 col-md-12 col-lg-4">Hospital-Pill</div>
+                        </div>
+                    </div>
                 </div>
             </Layout>
         </div>
